8acb832da895c0898d4c8ca08e495b2171942d64
yspv <yusupovamirxon@yahoo.com>
2025-11-06 18:38:12 +0500

    feat: link brand to product

---
+++ src/links/product-brand.ts
@@ -0,0 +1,11 @@
+import { defineLink } from "@medusajs/framework/utils";
+import ProductModule from "@medusajs/medusa/product";
+import BrandModule from "../modules/brand";
+
+export default defineLink(
+  {
+    linkable: ProductModule.linkable.product,
+    isList: true,
+  },
+  BrandModule.linkable.brand,
+);
