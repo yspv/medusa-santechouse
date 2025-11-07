c5b9675dac10ff63eefcc723885bc9cec65629c8
yspv <yusupovamirxon@yahoo.com>
2025-11-07 20:17:38 +0500

    feat: admin components

---
+++ src/admin/components/modals/route-modal-provider/index.ts
@@ -0,0 +1,3 @@
+export * from "./route-modal-context";
+export * from "./route-modal-provider";
+export * from "./use-route-modal";
---
+++ src/admin/components/modals/stacked-modal-provider/index.ts
@@ -0,0 +1,3 @@
+export * from "./stacked-modal-context";
+export * from "./stacked-modal-provider";
+export * from "./use-stacked-modal";
