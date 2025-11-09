430b6f14fb610c848b637b238109639ee0a85e22
yspv <yusupovamirxon@yahoo.com>
2025-11-09 20:55:59 +0500

    chore: import missed api hooks

--- src/admin/hooks/api/index.ts
+++ src/admin/hooks/api/index.ts
@@ -1,3 +1,6 @@
 export * from "./brands";
 export * from "./product-tags";
 export * from "./product-types";
+export * from "./inventories";
+export * from "./products";
+export * from "./sales-channels";
