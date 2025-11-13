467f9405a80772f3929a0687f24da22d07154026
yspv <yusupovamirxon@yahoo.com>
2025-11-14 00:30:04 +0500

    feat: base cashback types

---
+++ src/types/cashback/common.ts
@@ -0,0 +1,73 @@
+import {
+  ProductVariantDTO,
+  MetadataType,
+  CustomerDTO,
+  BaseFilterable,
+} from "@medusajs/framework/types";
+
+export type CashbackTransactionType = "earned" | "redeemed" | "adjustment";
+
+export interface FilterableCashbackProps
+  extends BaseFilterable<FilterableCashbackProps> {
+  id?: string | string[];
+  is_active?: boolean;
+}
+
+export interface FilterableCashbackAmountProps
+  extends BaseFilterable<FilterableCashbackAmountProps> {
+  id?: string | string[];
+  currency_code?: string | string[];
+  is_active?: boolean;
+}
+
+export interface FilterableCashbackAccountProps
+  extends BaseFilterable<FilterableCashbackAccountProps> {
+  id?: string | string[];
+  currency_code?: string | string[];
+  is_active?: boolean;
+}
+
+export interface FilterableCashbackTransactionProps
+  extends BaseFilterable<FilterableCashbackTransactionProps> {
+  id?: string | string[];
+  type?: CashbackTransactionType | CashbackTransactionType[];
+  currency_code?: string | string[];
+}
+
+export interface CashbackDTO {
+  id: string;
+  product_varaint?: ProductVariantDTO | null;
+  amounts: CashbackAmountDTO[];
+  is_active: boolean;
+  metadata: MetadataType;
+}
+
+export interface CashbackAmountDTO {
+  id: string;
+  cashback?: CashbackDTO;
+  currency_code: string;
+  amount: string;
+  is_active: boolean;
+  metadata: MetadataType;
+}
+
+export interface CashbackAccountDTO {
+  id: string;
+  customer?: CustomerDTO;
+  transactions: CashbackTransactionDTO[];
+  currency_code: string;
+  total_earned: number;
+  total_redeemed: number;
+  balance: number;
+  is_active: boolean;
+  metadata: MetadataType;
+}
+
+export interface CashbackTransactionDTO {
+  id: string;
+  account?: CashbackAccountDTO;
+  cashback?: CashbackDTO;
+  currency_code: string;
+  amount: number;
+  metadata: MetadataType;
+}
---
+++ src/types/cashback/mutations.ts
@@ -0,0 +1,57 @@
+import { MetadataType } from "@medusajs/framework/types";
+import { CashbackTransactionType } from "./common";
+
+export interface CreateCashbackDTO {
+  amounts?: CreateCashbackAmountDTO[];
+  is_active?: boolean;
+  metadata?: MetadataType;
+}
+
+export interface CreateCashbackAmountDTO {
+  currency_code: string;
+  amount: number;
+  is_active?: boolean;
+  metadata?: MetadataType;
+}
+
+export interface CreateCashbackAccountDTO {
+  currency_code: string;
+  total_earned?: number;
+  total_redeemed?: number;
+  balance?: number;
+  is_active?: boolean;
+  metadata?: MetadataType;
+}
+
+export interface CreateCashbackTransactionDTO {
+  account_id: string;
+  reference_id?: string;
+  type: CashbackTransactionType;
+  currency_code: string;
+  amount: number;
+  metadata: MetadataType;
+}
+
+export interface UpdateCashbackDTO {
+  id: string;
+  is_active?: boolean;
+  metadata?: MetadataType;
+}
+
+export interface UpdateCashbackAmountDTO {
+  id: string;
+  currency_code?: string;
+  amount?: number;
+  is_active?: boolean;
+  metadata?: MetadataType;
+}
+
+export interface UpdateCashbackAccountDTO {
+  id: string;
+  currency_code?: string;
+  total_earned?: number;
+  total_redeemed?: number;
+  balance?: number;
+  is_active?: boolean;
+  metadata?: MetadataType;
+}
