856bff7e780fe66cd50a4ddc53fe37759da5b296
yspv <yusupovamirxon@yahoo.com>
2025-11-14 21:40:57 +0500

    refactor(cashback): link variant with created cashback

--- src/workflows/cashback/workflows/create-cashbacks.ts
+++ src/workflows/cashback/workflows/create-cashbacks.ts
@@ -1,15 +1,33 @@
 import { CreateCashbackDTO } from "@/types";
 import {
   createWorkflow,
+  transform,
+  WorkflowData,
   WorkflowResponse,
 } from "@medusajs/framework/workflows-sdk";
-import { createCashbacksStep } from "../steps";
+import { createCashbacksStep, createCashbackVariantLinkStep } from "../steps";
+
+export type CreateCashbackWorkflowInput = {
+  cashbacks: (CreateCashbackDTO & { variant_id: string })[];
+};
 
 export const createCashbacksWorkflowId = "create-cashbacks";
 export const createCashbacksWorkflow = createWorkflow(
   createCashbacksWorkflowId,
-  (input: CreateCashbackDTO[]) => {
-    const created = createCashbacksStep(input);
-    return new WorkflowResponse(created);
+  (input: WorkflowData<CreateCashbackWorkflowInput>) => {
+    const { cashbacks } = input;
+    const createdCashbacks = createCashbacksStep(cashbacks);
+    const cashbackAndVariantLinks = transform(
+      { createdCashbacks, input },
+      (data) => {
+        return data.createdCashbacks.map((cashback, index) => {
+          const variant_id = input.cashbacks[index].variant_id;
+          const cashback_id = cashback.id;
+          return { variant_id, cashback_id };
+        });
+      },
+    );
+    createCashbackVariantLinkStep({ links: cashbackAndVariantLinks });
+    return new WorkflowResponse(createdCashbacks);
   },
 );
