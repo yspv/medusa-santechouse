300d95eb46def7e6fb1980d5c9dcd203b20b2e42
yspv <yusupovamirxon@yahoo.com>
2025-11-16 19:23:00 +0500

    feat(cashback): create cashback accounts workflow

---
+++ src/workflows/cashback/workflows/create-cashback-accounts.ts
@@ -0,0 +1,33 @@
+import { CreateCashbackAccountDTO } from "@/types";
+import {
+  createWorkflow,
+  transform,
+  WorkflowData,
+  WorkflowResponse,
+} from "@medusajs/framework/workflows-sdk";
+import {
+  createCashbackAccountCustomerLinkStep,
+  createCashbackAccountsStep,
+} from "../steps";
+
+export type CreateCashbackAccountsWorkflowInput = {
+  accounts: (CreateCashbackAccountDTO & { customer_id: string })[];
+};
+
+export const createCashbackAccountsWorkflowId = "create-cashback-accounts";
+
+export const createCashbackAccountsWorkflow = createWorkflow(
+  createCashbackAccountsWorkflowId,
+  (input: WorkflowData<CreateCashbackAccountsWorkflowInput>) => {
+    const created = createCashbackAccountsStep(input.accounts);
+    const accountAndCustomerLinks = transform({ created, input }, (data) => {
+      return created.map((account, index) => {
+        const account_id = account.id;
+        const customer_id = data.input.accounts[index].customer_id;
+        return { account_id, customer_id };
+      });
+    });
+    createCashbackAccountCustomerLinkStep({ links: accountAndCustomerLinks });
+    return new WorkflowResponse(created);
+  },
+);
