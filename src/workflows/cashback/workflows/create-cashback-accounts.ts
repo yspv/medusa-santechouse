13cbd2df61e3b03c94398d23fa44ce287c3df619
yspv <yusupovamirxon@yahoo.com>
2025-11-21 18:30:33 +0500

    refactor(cashback): check duplicated currency code before create cashback account

--- src/workflows/cashback/workflows/create-cashback-accounts.ts
+++ src/workflows/cashback/workflows/create-cashback-accounts.ts
@@ -1,6 +1,8 @@
-import { CreateCashbackAccountDTO } from "@/types";
+import { CashbackAccountDTO, CreateCashbackAccountDTO } from "@/types";
 import {
+  createStep,
   createWorkflow,
+  StepResponse,
   transform,
   WorkflowData,
   WorkflowResponse,
@@ -9,9 +11,60 @@ import {
   createCashbackAccountCustomerLinkStep,
   createCashbackAccountsStep,
 } from "../steps";
+import {
+  ContainerRegistrationKeys,
+  MedusaError,
+} from "@medusajs/framework/utils";
 
 export type CreateCashbackAccountsWorkflowInput = {
   accounts: (CreateCashbackAccountDTO & { customer_id: string })[];
+};
+
+const validateCreateCashbackAccounts = createStep(
+  "validate-create-cashback-acccounts",
+  async (
+    data: CreateCashbackAccountsWorkflowInput["accounts"],
+    { container },
+  ) => {
+    const query = container.resolve(ContainerRegistrationKeys.QUERY);
+    const { data: accounts } = await query.graph({
+      entity: "customer_cashback_account",
+      filters: { customer_id: data.map((c) => c.customer_id) },
+      fields: ["*", "cashback_account.*"],
+    });
+    const existedAccounts = accounts.reduce((acc, account) => {
+      const customerId = account.customer_id;
+      const currencyCode = account.cashback_account!.currency_code;
+      const currencyCodes = acc.get(customerId);
+      if (currencyCodes) {
+        currencyCodes.push(currencyCode);
+      } else {
+        acc.set(customerId, [currencyCode]);
+      }
+      return acc;
+    }, new Map<string, string[]>());
+    const isDublicated = data.some((d) =>
+      existedAccounts.get(d.customer_id)?.includes(d.currency_code),
+    );
+    if (isDublicated) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "Duplicated cashback account currencies",
+      );
+    }
+    return new StepResponse(void 0);
+  },
+);
+
+const linkCustomerWithCashback = (data: {
+  created: CashbackAccountDTO[];
+  input: CreateCashbackAccountsWorkflowInput;
+}) => {
+  return data.created.map((account, index) => {
+    const account_id = account.id;
+    const customer_id = data.input.accounts[index].customer_id;
+    return { account_id, customer_id };
+  });
 };
 
 export const createCashbackAccountsWorkflowId = "create-cashback-accounts";
@@ -19,14 +72,12 @@ export const createCashbackAccountsWorkflowId = "create-cashback-accounts";
 export const createCashbackAccountsWorkflow = createWorkflow(
   createCashbackAccountsWorkflowId,
   (input: WorkflowData<CreateCashbackAccountsWorkflowInput>) => {
+    validateCreateCashbackAccounts(input.accounts);
     const created = createCashbackAccountsStep(input.accounts);
-    const accountAndCustomerLinks = transform({ created, input }, (data) => {
-      return data.created.map((account, index) => {
-        const account_id = account.id;
-        const customer_id = data.input.accounts[index].customer_id;
-        return { account_id, customer_id };
-      });
-    });
+    const accountAndCustomerLinks = transform(
+      { created, input },
+      linkCustomerWithCashback,
+    );
     createCashbackAccountCustomerLinkStep({ links: accountAndCustomerLinks });
     return new WorkflowResponse(created);
   },
