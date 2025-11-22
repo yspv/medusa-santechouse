66890f2e6febde0805f5657676b39ef598c79a55
yspv <yusupovamirxon@yahoo.com>
2025-11-23 01:21:45 +0500

    chore(price-conversion): add admin middlewares

--- src/api/admin/middlewares.ts
+++ src/api/admin/middlewares.ts
@@ -3,10 +3,12 @@ import { brandsMiddlewares } from "./brands/middlewares";
 import { productsMiddlewares } from "./products/middlewares";
 import { cashbackMiddlewares } from "./cashbacks/middlewares";
 import { cashbackAccountMiddlewares } from "./cashback-accounts/middlewares";
+import { priceConversionMiddlewares } from "./price-conversions/middlewares";
 
 export const adminMiddlewares: MiddlewareRoute[] = [
   ...brandsMiddlewares,
   ...cashbackMiddlewares,
   ...productsMiddlewares,
   ...cashbackAccountMiddlewares,
+  ...priceConversionMiddlewares,
 ];
