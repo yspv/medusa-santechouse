f3c0da0d0c0296654eed877a8186259cf7c76b85
yspv <yusupovamirxon@yahoo.com>
2025-11-23 01:20:32 +0500

    feat(price-conversion): admin get price conversion list

--- src/api/admin/price-conversions/route.ts
+++ src/api/admin/price-conversions/route.ts
@@ -1,5 +1,34 @@
+import {
+  AdminPriceConversionListResponse,
+  AdminPriceConversionParams,
+} from "@/types/http/price-conversion";
 import { priceConversionWorkflow } from "@/workflows/price-conversion/workflows";
-import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
+import {
+  MedusaRequest,
+  MedusaResponse,
+  refetchEntities,
+} from "@medusajs/framework";
+
+export const GET = async (
+  req: MedusaRequest<AdminPriceConversionParams>,
+  res: MedusaResponse<AdminPriceConversionListResponse>,
+) => {
+  const { data, metadata } = await refetchEntities({
+    entity: "price_conversion",
+    idOrFilter: req.filterableFields,
+    scope: req.scope,
+    fields: req.queryConfig.fields,
+    pagination: req.queryConfig.pagination,
+    withDeleted: req.queryConfig.withDeleted,
+  });
+  console.log(data);
+  res.json({
+    price_conversions: data,
+    count: metadata.count,
+    limit: metadata.take,
+    offset: metadata.skip,
+  });
+};
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   await priceConversionWorkflow(req.scope).run({
