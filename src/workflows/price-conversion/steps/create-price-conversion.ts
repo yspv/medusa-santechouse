a4fd629ea1f28dc5bea733e7d531cf83668cfa72
yspv <yusupovamirxon@yahoo.com>
2025-11-23 00:49:25 +0500

    refactor(price-conversion): update price set in the step

--- src/workflows/price-conversion/steps/create-price-conversion.ts
+++ src/workflows/price-conversion/steps/create-price-conversion.ts
@@ -1,13 +1,66 @@
 import { PRICE_CONVERSION_MODULE } from "@/modules/price-conversion";
 import { CreatePriceConversionDTO } from "@/types";
+import { Modules } from "@medusajs/framework/utils";
 import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk";
+import { PriceConversion } from "../../../../.medusa/types/query-entry-points";
+import {
+  UpsertPriceSetDTO,
+  PriceSetDTO,
+  PriceDTO,
+  MoneyAmountDTO,
+} from "@medusajs/framework/types";
+
+const mapPriceSet = (sets: PriceSetDTO[]) => {
+  const mapPrice = (prices: MoneyAmountDTO[]) => {
+    return prices.reduce((acc, price) => {
+      acc[price.currency_code!] = price;
+      return acc;
+    }, {});
+  };
+  const maped = sets.reduce((acc, set) => {
+    acc.set(set.id, mapPrice(set.prices || []));
+    return acc;
+  }, new Map());
+  return maped;
+};
+
+const convertPrice = (data: {
+  price_sets: Map<string, Record<string, any>>;
+  conversion: PriceConversion;
+}): UpsertPriceSetDTO[] => {
+  const { from, to, rate } = data.conversion;
+  console.log("Price set", data.price_sets);
+  const converted = Array.from(data.price_sets).reduce((acc, [id, prices]) => {
+    const sourcePrice = prices[from];
+    if (!sourcePrice) return acc;
+    if (prices[to]) {
+      prices[to].amount = sourcePrice.amount * rate;
+    } else {
+      prices[to] = {
+        currency_code: to,
+        amount: sourcePrice.amount * rate,
+      };
+    }
+    acc.push({ id, prices: Object.values(prices) });
+    return acc;
+  }, [] as UpsertPriceSetDTO[]);
+  return converted;
+};
 
 export const createPriceConversionStepId = "price-conversion";
 export const createPriceConversionStep = createStep(
   createPriceConversionStepId,
   async (data: CreatePriceConversionDTO, { container }) => {
     const service = container.resolve(PRICE_CONVERSION_MODULE);
+    const price = container.resolve(Modules.PRICING);
     const priceConversion = await service.createPriceConversions(data);
+    const sets = await price.listPriceSets({}, { relations: ["prices.*"] });
+    const mapped = mapPriceSet(sets);
+    const converted = convertPrice({
+      price_sets: mapped,
+      conversion: priceConversion,
+    });
+    await price.upsertPriceSets(converted);
     return new StepResponse(priceConversion, priceConversion.id);
   },
   async (id, { container }) => {
--- src/workflows/price-conversion/workflows/price-conversion.ts
+++ src/workflows/price-conversion/workflows/price-conversion.ts
@@ -2,27 +2,69 @@ import {
   createStep,
   createWorkflow,
   StepResponse,
+  transform,
   WorkflowData,
   WorkflowResponse,
 } from "@medusajs/framework/workflows-sdk";
 import { updatePriceSetsStep } from "@medusajs/medusa/core-flows";
 import { createPriceConversionStep } from "../steps";
 import { CreatePriceConversionDTO } from "@/types";
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
+import {
+  Price,
+  PriceConversion,
+  PriceSet,
+} from "../../../../.medusa/types/query-entry-points";
+import { UpsertPriceSetDTO } from "@medusajs/framework/types";
 
-const getAllPrices = createStep(
-  "get-all-prices",
-  async (data: { currency_code: string }, { container }) => {
-    const query = container.resolve("query");
-    const { data: prices } = await query.graph({
-      entity: "price",
-      fields: ["*"],
-      filters: {
-        currency_code: data.currency_code,
-      },
+const getAllPriceSets = createStep(
+  "get-all-price-sets",
+  async (data, { container }) => {
+    const query = container.resolve(ContainerRegistrationKeys.QUERY);
+    const { data: priceSets } = await query.graph({
+      entity: "price_set",
+      fields: ["*", "prices.*"],
     });
-    return new StepResponse(prices);
+    return new StepResponse(priceSets);
   },
 );
+
+const mapPriceSet = (data: { price_sets: PriceSet[] }) => {
+  const mapPrice = (data: { prices: Price[] }) => {
+    return data.prices.reduce((acc, price) => {
+      acc[price.currency_code] = price;
+      return acc;
+    }, {});
+  };
+  const maped = data.price_sets.reduce((acc, set) => {
+    acc.set(set.id, mapPrice({ prices: set.prices as Price[] }));
+    return acc;
+  }, new Map());
+  return maped;
+};
+
+const convertPrice = (data: {
+  price_sets: Map<string, Record<string, any>>;
+  conversion: PriceConversion;
+}): UpsertPriceSetDTO[] => {
+  const { from, to, rate } = data.conversion;
+  console.log("Price set", data.price_sets);
+  const converted = Array.from(data.price_sets).reduce((acc, [id, prices]) => {
+    const sourcePrice = prices[from];
+    if (!sourcePrice) return acc;
+    if (prices[to]) {
+      prices[to].amount = sourcePrice.amount * rate;
+    } else {
+      prices[to] = {
+        currency_code: to,
+        amount: sourcePrice.amount * rate,
+      };
+    }
+    acc.push({ id, prices: Object.values(prices) });
+    return acc;
+  }, [] as UpsertPriceSetDTO[]);
+  return converted;
+};
 
 export type PriceConversionWorkflowInput = {
   price_conversion: CreatePriceConversionDTO;
@@ -32,19 +74,7 @@ export const priceConversionWorkflowId = "price-conversion";
 export const priceConversionWorkflow = createWorkflow(
   priceConversionWorkflowId,
   (input: WorkflowData<PriceConversionWorkflowInput>) => {
-    const prices = getAllPrices({ currency_code: "usd" });
     const conversion = createPriceConversionStep(input.price_conversion);
-    updatePriceSetsStep({
-      price_sets: prices.map((price) => ({
-        id: price.price_set_id,
-        prices: [
-          {
-            currency_code: conversion.to,
-            amount: price.amount * conversion.rate,
-          },
-        ],
-      })),
-    });
     return new WorkflowResponse(conversion);
   },
 );
