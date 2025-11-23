c9914a486ec334ef427dff2e46f1fd21b06cd5dc
yspv <yusupovamirxon@yahoo.com>
2025-11-23 05:20:09 +0500

    feat(admin): cashback amount cell

---
+++ src/admin/components/table/table-cells/cashback/cashback-amounts/cashback-amounts.tsx
@@ -0,0 +1,58 @@
+import { useTranslation } from "react-i18next";
+import { AdminCashbackAmount } from "../../../../../../types";
+import { PlaceholderCell } from "../../common/placeholder-cell";
+import { Tooltip } from "@medusajs/ui";
+import { localizePrice } from "../../../../../lib/localize-price";
+
+type CashbackAmountsCellProps = {
+  amounts?: AdminCashbackAmount[] | null;
+};
+
+export const CashbackAmountsCell = ({ amounts }: CashbackAmountsCellProps) => {
+  const { t, i18n } = useTranslation();
+
+  if (!amounts || !amounts.length) {
+    return <PlaceholderCell />;
+  }
+
+  if (amounts.length > 1) {
+    return (
+      <div className="flex h-full w-full items-center gap-x-1 overflow-hidden">
+        <span className="truncate">
+          {amounts
+            .slice(0, 1)
+            .map((sc) => sc.currency_code.toUpperCase())
+            .join(", ")}
+        </span>
+        <Tooltip
+          content={
+            <ul>
+              {amounts.slice(1).map((sc) => (
+                <li key={sc.id}>{sc.currency_code.toUpperCase()}</li>
+              ))}
+            </ul>
+          }
+        >
+          <span className="text-xs">
+            {t("general.plusCountMore", {
+              count: amounts.length - 1,
+            })}
+          </span>
+        </Tooltip>
+      </div>
+    );
+  }
+
+  const amount = amounts[0];
+
+  return (
+    <div className="txt-small text-ui-fg-subtle">
+      <span>
+        {localizePrice(i18n.language, {
+          amount: amount.amount,
+          currency_code: amount.currency_code,
+        })}
+      </span>
+    </div>
+  );
+};
