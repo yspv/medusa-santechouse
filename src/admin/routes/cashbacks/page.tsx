e79f64fa57109d4527123cfbcdbe67020811da6b
yspv <yusupovamirxon@yahoo.com>
2025-11-23 05:40:04 +0500

    feat(admin): cashback sidebar icon

--- src/admin/routes/cashbacks/page.tsx
+++ src/admin/routes/cashbacks/page.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@medusajs/ui";
 import { defineRouteConfig } from "@medusajs/admin-sdk";
 import { CashbackListTable } from "./components/cashback-list-table/cashback-list-table";
+import { Cash } from "@medusajs/icons";
 
 const CashbacksListPage = () => {
   return (
@@ -12,6 +13,7 @@ const CashbacksListPage = () => {
 
 export const config = defineRouteConfig({
   label: "Кэшбек",
+  icon: Cash,
 });
 
 export default CashbacksListPage;
