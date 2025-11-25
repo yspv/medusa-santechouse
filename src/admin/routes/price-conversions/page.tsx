e60da4a00ae0b94ced17fc48d7c259218b60f1e8
yspv <yusupovamirxon@yahoo.com>
2025-11-25 07:03:28 +0500

    feat(admin): price conversion router outlet

--- src/admin/routes/price-conversions/page.tsx
+++ src/admin/routes/price-conversions/page.tsx
@@ -2,12 +2,17 @@ import { Container } from "@medusajs/ui";
 import { defineRouteConfig } from "@medusajs/admin-sdk";
 import { ArrowPath } from "@medusajs/icons";
 import { PriceConversionsListTable } from "./components/price-conversions-list-table/price-conversions-list-table";
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
 
 const PriceConversionsPage = () => {
   return (
-    <Container className="divide-y p-0">
-      <PriceConversionsListTable />
-    </Container>
+    <Fragment>
+      <Container className="divide-y p-0">
+        <PriceConversionsListTable />
+      </Container>
+      <Outlet />
+    </Fragment>
   );
 };
 
