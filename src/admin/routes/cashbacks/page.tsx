ca97a1b7b0f3fe1a087c8e88c197c8921278fe31
yspv <yusupovamirxon@yahoo.com>
2025-11-25 23:39:27 +0500

    feat(admin): add outlet in cashback page

--- src/admin/routes/cashbacks/page.tsx
+++ src/admin/routes/cashbacks/page.tsx
@@ -2,12 +2,17 @@ import { Container } from "@medusajs/ui";
 import { defineRouteConfig } from "@medusajs/admin-sdk";
 import { CashbackListTable } from "./components/cashback-list-table/cashback-list-table";
 import { Cash } from "@medusajs/icons";
+import { Outlet } from "react-router-dom";
+import React from "react";
 
 const CashbacksListPage = () => {
   return (
-    <Container className="divide-y p-0">
-      <CashbackListTable />
-    </Container>
+    <React.Fragment>
+      <Container className="divide-y p-0">
+        <CashbackListTable />
+      </Container>
+      <Outlet />
+    </React.Fragment>
   );
 };
 
