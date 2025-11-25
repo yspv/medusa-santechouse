6bd2ff11783b3b41e09c75cb209a3f4c7d1dae82
yspv <yusupovamirxon@yahoo.com>
2025-11-25 07:30:17 +0500

    feat(admin): price conversion list add breadcrumb

--- src/admin/routes/price-conversions/page.tsx
+++ src/admin/routes/price-conversions/page.tsx
@@ -4,6 +4,7 @@ import { ArrowPath } from "@medusajs/icons";
 import { PriceConversionsListTable } from "./components/price-conversions-list-table/price-conversions-list-table";
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
+import { t } from "i18next";
 
 const PriceConversionsPage = () => {
   return (
@@ -20,4 +21,9 @@ export const config = defineRouteConfig({
   label: "Конвертация цены",
   icon: ArrowPath,
 });
+
+export const handle = {
+  breadcrumb: () => t("price-conversions.domain"),
+};
+
 export default PriceConversionsPage;
