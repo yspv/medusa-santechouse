d27a49d989094418ab2c4dee968a56e5cbf415e1
yspv <yusupovamirxon@yahoo.com>
2025-11-26 21:56:04 +0500

    refactor(admin): remove widgets props from TwoColumnsPage layout

--- src/admin/components/layout/pages/two-column-page/two-column-page.tsx
+++ src/admin/components/layout/pages/two-column-page/two-column-page.tsx
@@ -1,62 +1,32 @@
 import { clx } from "@medusajs/ui";
-import { Children, ComponentPropsWithoutRef, ComponentType } from "react";
+import { Children, ComponentPropsWithoutRef } from "react";
 import { Outlet } from "react-router-dom";
 import { JsonViewSection } from "../../../common/json-view-section";
 import { MetadataSection } from "../../../common/metadata-section";
-import { PageProps, WidgetProps } from "../types";
-
-interface TwoColumnWidgetProps extends WidgetProps {
-  sideBefore: ComponentType<any>[];
-  sideAfter: ComponentType<any>[];
-}
+import { PageProps } from "../types";
+import { TwoColumnPageSkeleton } from "@/components/common/skeleton";
 
 interface TwoColumnPageProps<TData> extends PageProps<TData> {
-  widgets: TwoColumnWidgetProps;
+  mainSections?: number;
+  sidebarSections?: number;
+  isLoading?: boolean;
 }
 
 const Root = <TData,>({
   children,
-  /**
-   * Widgets to be rendered in the main content area and sidebar.
-   */
-  widgets,
-  /**
-   * Data to be passed to widgets, JSON view, and Metadata view.
-   */
   data,
-  /**
-   * Whether to show JSON view of the data. Defaults to false.
-   */
+  mainSections,
+  sidebarSections,
+  isLoading,
   showJSON = false,
-  /**
-   * Whether to show metadata view of the data. Defaults to false.
-   */
   showMetadata = false,
-  /**
-   * Whether to render an outlet for children routes. Defaults to true.
-   */
   hasOutlet = true,
 }: TwoColumnPageProps<TData>) => {
-  const widgetProps = { data };
-  const { before, after, sideBefore, sideAfter } = widgets;
-
   if (showJSON && !data) {
-    if (process.env.NODE_ENV === "development") {
-      console.warn(
-        "`showJSON` is true but no data is provided. To display JSON, provide data prop.",
-      );
-    }
-
     showJSON = false;
   }
 
   if (showMetadata && !data) {
-    if (process.env.NODE_ENV === "development") {
-      console.warn(
-        "`showMetadata` is true but no data is provided. To display metadata, provide data prop.",
-      );
-    }
-
     showMetadata = false;
   }
 
@@ -71,15 +41,9 @@ const Root = <TData,>({
 
   return (
     <div className="flex w-full flex-col gap-y-3">
-      {before.map((Component, i) => {
-        return <Component {...widgetProps} key={i} />;
-      })}
       <div className="flex w-full flex-col items-start gap-x-4 gap-y-3 xl:grid xl:grid-cols-[minmax(0,_1fr)_440px]">
         <div className="flex w-full min-w-0 flex-col gap-y-3">
           {main}
-          {after.map((Component, i) => {
-            return <Component {...widgetProps} key={i} />;
-          })}
           {showExtraData && (
             <div className="hidden flex-col gap-y-3 xl:flex">
               {showMetadata && <MetadataSection data={data!} />}
@@ -88,13 +52,7 @@ const Root = <TData,>({
           )}
         </div>
         <div className="flex w-full flex-col gap-y-3 xl:mt-0">
-          {sideBefore.map((Component, i) => {
-            return <Component {...widgetProps} key={i} />;
-          })}
           {sidebar}
-          {sideAfter.map((Component, i) => {
-            return <Component {...widgetProps} key={i} />;
-          })}
           {showExtraData && (
             <div className="flex flex-col gap-y-3 xl:hidden">
               {showMetadata && <MetadataSection data={data!} />}
