78c283a7c4fa7d0e7ffbac718aa088ac75c9bdfb
yspv <yusupovamirxon@yahoo.com>
2025-11-26 23:02:04 +0500

    feat(admin): cashback edit page

---
+++ src/admin/routes/cashbacks/[id]/@edit/page.tsx
@@ -0,0 +1,28 @@
+import { Heading } from "@medusajs/ui";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+import { RouteDrawer } from "@/components/modals/route-drawer/route-drawer";
+import { useCashback } from "@/hooks/api";
+import { CashbackEditForm } from "../../components/cashback-edit-form";
+
+const CashbackEditPage = () => {
+  const { id } = useParams();
+  const { t } = useTranslation();
+  const { cashback, isLoading, isError, error } = useCashback(id as string);
+  if (isError) {
+    throw error;
+  }
+  return (
+    <RouteDrawer>
+      <RouteDrawer.Header>
+        <RouteDrawer.Title asChild>
+          <Heading>{t("cashbacks.edit.header")}</Heading>
+        </RouteDrawer.Title>
+        <RouteDrawer.Description className="sr-only"></RouteDrawer.Description>
+      </RouteDrawer.Header>
+      {!isLoading && cashback && <CashbackEditForm cashback={cashback} />}
+    </RouteDrawer>
+  );
+};
+
+export default CashbackEditPage;
