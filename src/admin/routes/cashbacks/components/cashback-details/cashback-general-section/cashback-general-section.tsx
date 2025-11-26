811851dc6feb8646fa45aea7e7dacdcc7ef09750
yspv <yusupovamirxon@yahoo.com>
2025-11-26 22:40:13 +0500

    feat(admin): cashback general section component

---
+++ src/admin/routes/cashbacks/components/cashback-details/cashback-general-section/cashback-general-section.tsx
@@ -0,0 +1,37 @@
+import { Container, Heading, StatusBadge } from "@medusajs/ui";
+import { AdminCashback } from "@/types";
+import { useTranslation } from "react-i18next";
+import { getIsActiveProps } from "@/lib/get-status-props";
+import { SectionRow } from "@/components/common/section";
+import { useDate } from "@/hooks/use-date";
+import { ActionMenu } from "@/components/common/action-menu";
+
+export const CashbackGeneralSection = (props: { cashback: AdminCashback }) => {
+  const { cashback } = props;
+  const { product_variant: variant } = cashback;
+  const { t } = useTranslation();
+  const { getFullDate } = useDate();
+  const activeProps = getIsActiveProps(cashback.is_active, t);
+  return (
+    <Container className="divide-y p-0">
+      <div className="flex items-center justify-between px-6 py-6">
+        <Heading level="h2">
+          {cashback.product_variant?.sku} / {t("fields.details")}
+        </Heading>
+        <div className="flex items-center gap-x-4">
+          <div className="flex items-center gap-x-2">
+            <StatusBadge color={activeProps.color}>
+              {activeProps.label}
+            </StatusBadge>
+          </div>
+          <ActionMenu groups={[]} />
+        </div>
+      </div>
+      <SectionRow title={t("fields.sku")} value={variant?.sku} />
+      <SectionRow
+        title={t("fields.createdAt")}
+        value={getFullDate({ date: cashback.created_at })}
+      />
+    </Container>
+  );
+};
