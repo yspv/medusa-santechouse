2757a5e6427416b091d535fb19b013b0ff6c2098
yspv <yusupovamirxon@yahoo.com>
2025-11-26 22:43:03 +0500

    feat(admin): cashback detials page

---
+++ src/admin/routes/cashbacks/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { useCashback } from "@/hooks/api";
+import { useParams } from "react-router-dom";
+import { TwoColumnPage } from "@/components/layout/pages";
+import { TwoColumnPageSkeleton } from "@/components/common/skeleton";
+import {
+  CashbackGeneralSection,
+  CashbackAmountsSection,
+} from "../components/cashback-details";
+
+const CashbackDetailsPage = () => {
+  const { id } = useParams();
+  const { cashback, isLoading, isError, error } = useCashback(id as string, {
+    fields: "+product_variant.*",
+  });
+  if (isError) {
+    throw error;
+  }
+  if (isLoading || !cashback) {
+    return <TwoColumnPageSkeleton mainSections={1} showJSON />;
+  }
+  return (
+    <TwoColumnPage isLoading={isLoading} data={cashback} showJSON>
+      <TwoColumnPage.Main>
+        <CashbackGeneralSection cashback={cashback} />
+      </TwoColumnPage.Main>
+      <TwoColumnPage.Sidebar>
+        <CashbackAmountsSection amounts={cashback.amounts || []} />
+      </TwoColumnPage.Sidebar>
+    </TwoColumnPage>
+  );
+};
+
+export default CashbackDetailsPage;
