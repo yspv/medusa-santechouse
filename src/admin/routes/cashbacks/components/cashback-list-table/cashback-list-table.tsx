714ad080d70ab9ea32bef551541fe82daf77b7ed
yspv <yusupovamirxon@yahoo.com>
2025-11-28 04:20:15 +0500

    refactor(admin): change cashback table components

--- src/admin/routes/cashbacks/components/cashback-list-table/cashback-list-table.tsx
+++ src/admin/routes/cashbacks/components/cashback-list-table/cashback-list-table.tsx
@@ -1,63 +1,43 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import {
-  Button,
-  createDataTableColumnHelper,
-  DataTable,
-  DataTablePaginationState,
-  Heading,
-  useDataTable,
-} from "@medusajs/ui";
-import { Link, useNavigate } from "react-router-dom";
-import { useCashbackTableQuery } from "./use-cashback-table-query";
 import { useCashbacks } from "../../../../hooks/api/cashback";
 import { keepPreviousData } from "@tanstack/react-query";
 import { AdminCashback } from "../../../../../types/http";
-import { useCashbackTableColumns } from "./use-cashback-table-columns";
+import {
+  cashbackColumnHelper,
+  useCashbackTableColumns,
+} from "@/hooks/table/columns";
 import { ActionMenu } from "../../../../components/common/action-menu";
 import { PencilSquare, Trash } from "@medusajs/icons";
+import { DataTable } from "@/components/common/data-table";
+import { useCashbackTableQuery } from "@/hooks/table/query";
 
 const PAGE_SIZE = 20;
 
 export const CashbackListTable = () => {
   const { t } = useTranslation();
-  const [pagination, setPagination] = React.useState<DataTablePaginationState>({
-    pageSize: PAGE_SIZE,
-    pageIndex: 0,
-  });
-  const navigate = useNavigate();
   const { searchParams } = useCashbackTableQuery({ pageSize: PAGE_SIZE });
-
-  const query = searchParams;
-
   const { cashbacks, count, isLoading } = useCashbacks(
-    { ...query },
+    { ...searchParams },
     { placeholderData: keepPreviousData },
   );
-
   const columns = useColumns();
-
-  const table = useDataTable({
-    columns,
-    data: cashbacks || [],
-    getRowId: (row) => row.id,
-    rowCount: count || 0,
-    onRowClick: (_, row) => navigate(`${row.id}`),
-    isLoading,
-    pagination: {
-      state: pagination,
-      onPaginationChange: setPagination,
-    },
-  });
-
   return (
-    <DataTable instance={table}>
-      <DataTable.Toolbar>
-        <Heading>{t("cashbacks.domain")}</Heading>
-      </DataTable.Toolbar>
-      <DataTable.Table />
-      <DataTable.Pagination />
-    </DataTable>
+    <DataTable
+      heading={t("cashbacks.domain")}
+      columns={columns}
+      data={cashbacks}
+      isLoading={isLoading}
+      getRowId={(row) => row.id}
+      pageSize={PAGE_SIZE}
+      rowCount={count}
+      layout="fill"
+      rowHref={(row) => `${row.id}`}
+      action={{
+        label: t("actions.create"),
+        to: "create",
+      }}
+    />
   );
 };
 
@@ -85,13 +65,12 @@ const CashbackActions = ({ cashback }: { cashback: AdminCashback }) => {
   );
 };
 
-const columnHelper = createDataTableColumnHelper<AdminCashback>();
 const useColumns = () => {
   const base = useCashbackTableColumns();
   return React.useMemo(
     () => [
       ...base,
-      columnHelper.display({
+      cashbackColumnHelper.display({
         id: "actions",
         cell: ({ row }) => <CashbackActions cashback={row.original} />,
       }),
