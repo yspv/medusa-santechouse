f582e13db330601010d483c672a74df743b21a5d
yspv <yusupovamirxon@yahoo.com>
2025-11-28 01:19:19 +0500

    feat(cashback): cashback module filterable fields

--- src/links/product-variant-cashback.ts
+++ src/links/product-variant-cashback.ts
@@ -2,7 +2,7 @@ import { defineLink } from "@medusajs/framework/utils";
 import ProductModule from "@medusajs/medusa/product";
 import CashbackModule from "@/modules/cashback";
 
-export default defineLink(
-  ProductModule.linkable.productVariant,
-  CashbackModule.linkable.cashback,
-);
+export default defineLink(ProductModule.linkable.productVariant, {
+  linkable: CashbackModule.linkable.cashback,
+  filterable: ["id"],
+});
