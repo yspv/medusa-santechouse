45e723ee0b2b8e3a4270fc643c5cd6faade258b1
yspv <yusupovamirxon@yahoo.com>
2025-11-28 07:30:08 +0500

    refactor(admin): use custom data table

--- src/admin/routes/cashbacks/accounts/components/cashback-account-list-table/cashback-account-list-table.tsx
+++ src/admin/routes/cashbacks/accounts/components/cashback-account-list-table/cashback-account-list-table.tsx
@@ -1,63 +1,41 @@
 import React from "react";
-import {
-  createDataTableColumnHelper,
-  DataTable,
-  DataTablePaginationState,
-  Heading,
-  useDataTable,
-} from "@medusajs/ui";
+import { createDataTableColumnHelper } from "@medusajs/ui";
 import { useCashbackAccountColumns } from "@/hooks/table/columns";
 import { AdminCashbackAccount } from "@/types";
 import { ActionMenu } from "@/components/common/action-menu";
 import { useTranslation } from "react-i18next";
 import { useCashbackAccounts } from "@/hooks/api";
 import { keepPreviousData } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
 import { useCashbackAccountTableQuery } from "@/hooks/table/query";
+import { DataTable } from "@/components/common/data-table";
 
 const PAGE_SIZE = 20;
 
 export const CashbackAccountListTable = () => {
   const { t } = useTranslation();
-  const [pagination, setPagination] = React.useState<DataTablePaginationState>({
-    pageSize: PAGE_SIZE,
-    pageIndex: 0,
-  });
-  const navigate = useNavigate();
+
   const { searchParams } = useCashbackAccountTableQuery({
     pageSize: PAGE_SIZE,
   });
 
-  const query = searchParams;
-
   const { cashback_accounts, count, isLoading } = useCashbackAccounts(
-    { ...query },
+    { ...searchParams },
     { placeholderData: keepPreviousData },
   );
 
   const columns = useColumns();
 
-  const table = useDataTable({
-    columns,
-    data: cashback_accounts || [],
-    getRowId: (row) => row.id,
-    rowCount: count || 0,
-    onRowClick: (_, row) => navigate(`${row.id}`),
-    isLoading,
-    pagination: {
-      state: pagination,
-      onPaginationChange: setPagination,
-    },
-  });
-
   return (
-    <DataTable instance={table}>
-      <DataTable.Toolbar>
-        <Heading>{t("cashback-accounts.domain")}</Heading>
-      </DataTable.Toolbar>
-      <DataTable.Table />
-      <DataTable.Pagination />
-    </DataTable>
+    <DataTable
+      heading={t("cashback-accounts.domain")}
+      columns={columns}
+      data={cashback_accounts}
+      isLoading={isLoading}
+      getRowId={(row) => row.id}
+      pageSize={PAGE_SIZE}
+      rowCount={count}
+      enableSearch={false}
+    />
   );
 };
 
