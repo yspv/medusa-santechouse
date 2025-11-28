be58de096332ff5df345341424c0f132f3a4d6ef
yspv <yusupovamirxon@yahoo.com>
2025-11-28 13:08:35 +0500

    fix(admin): cashback transaction query transaction type format

--- src/admin/hooks/table/query/use-cashback-transaction-table-query.ts
+++ src/admin/hooks/table/query/use-cashback-transaction-table-query.ts
@@ -10,12 +10,14 @@ export const useCashbackTransactionTableQuery = ({
   pageSize?: number;
   prefix?: string;
 }) => {
-  const raw = useQueryParams(["offset", "order"], prefix);
+  const raw = useQueryParams(["offset", "order", "account_id", "type"], prefix);
   const searchParams = {
     fields: DEFAULT_FIELDS,
     limit: pageSize,
     offset: raw.offset ? Number(raw.offset) : 0,
     order: raw.order || "-created_at",
+    account_id: raw.account_id,
+    type: raw.type ? JSON.parse(raw.type) : undefined,
   };
   return {
     raw,
