ff85b5e4cb14618bcce24f519aac508288ba03fc
yspv <yusupovamirxon@yahoo.com>
2025-11-28 10:46:30 +0500

    feat(admin): cashback transactions extend filterable fields

--- src/api/admin/cashback-transactions/validators.ts
+++ src/api/admin/cashback-transactions/validators.ts
@@ -1,6 +1,12 @@
 import { createFindParams } from "@medusajs/medusa/api/utils/validators";
+import z from "zod";
+
+export const AdminCashbackTransactionsParamsFields = z.object({
+  account_id: z.string().optional(),
+  type: z.string().optional(),
+});
 
 export const AdminCashbackTransactionsParams = createFindParams({
   offset: 0,
   limit: 50,
-});
+}).merge(AdminCashbackTransactionsParamsFields);
