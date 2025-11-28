83993955f9aaba8dbd6daf5c2837d217b9d400c7
yspv <yusupovamirxon@yahoo.com>
2025-11-28 06:54:03 +0500

    feat(admin): create cashback page

---
+++ src/admin/routes/cashbacks/@create/page.tsx
@@ -0,0 +1,11 @@
+import { RouteFocusModal } from "@/components/modals/route-focus-modal";
+import { CashbackCreateForm } from "../components/cashback-create-form";
+
+const CashbackCreatePage = () => {
+  return (
+    <RouteFocusModal>
+      <CashbackCreateForm />
+    </RouteFocusModal>
+  );
+};
+export default CashbackCreatePage;
