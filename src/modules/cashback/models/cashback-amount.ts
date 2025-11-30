a87efdf20c8e361cac7a99fd8d04bc0ed8e8c241
yspv <yusupovamirxon@yahoo.com>
2025-12-01 02:14:45 +0500

    refactor(cashback): unique cashback amount currency

--- src/modules/cashback/models/cashback-amount.ts
+++ src/modules/cashback/models/cashback-amount.ts
@@ -1,13 +1,21 @@
 import { model } from "@medusajs/framework/utils";
 import { Cashback } from "./cashback";
 
-export const CashbackAmount = model.define("cashback_amount", {
-  id: model.id({ prefix: "cbm" }).primaryKey(),
-  cashback: model.belongsTo(() => Cashback, {
-    mappedBy: "amounts",
-  }),
-  currency_code: model.text(),
-  amount: model.bigNumber(),
-  is_active: model.boolean().default(false),
-  metadata: model.json().nullable(),
-});
+export const CashbackAmount = model
+  .define("cashback_amount", {
+    id: model.id({ prefix: "cbm" }).primaryKey(),
+    cashback: model.belongsTo(() => Cashback, {
+      mappedBy: "amounts",
+    }),
+    currency_code: model.text(),
+    amount: model.bigNumber(),
+    is_active: model.boolean().default(false),
+    metadata: model.json().nullable(),
+  })
+  .indexes([
+    {
+      name: "IDX_cashback_account_cashback_id_currency_code_unique",
+      on: ["cashback_id", "currency_code"],
+      unique: true,
+    },
+  ]);
