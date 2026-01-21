fb84c484a12cdc320b9efe559cbb2648e210949c
yspv <yusupovamirxon@yahoo.com>
2026-01-21 21:11:37 +0500

    feat(api,store): allow fetching customer cashback account field

--- src/api/store/middlewares.ts
+++ src/api/store/middlewares.ts
@@ -1,8 +1,10 @@
 import { MiddlewareRoute } from "@medusajs/framework";
 import { storeBrandMiddlewares } from "./brands/middlewares";
 import { storeLocaleMiddlewares } from "./[locale]/middlewares";
+import { customerMiddlewares } from "./customers/middlewares";
 
 export const storeMiddlewares: MiddlewareRoute[] = [
   ...storeBrandMiddlewares,
   ...storeLocaleMiddlewares,
+  ...customerMiddlewares,
 ];
