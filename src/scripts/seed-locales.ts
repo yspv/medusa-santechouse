890f8b40cad8891db2f7f20ef10048551dabd39e
yspv <yusupovamirxon@yahoo.com>
2026-04-07 15:15:50 +0500

    chore: remove seed scripts

--- src/scripts/seed-locales.ts
+++ src/scripts/seed-locales.ts
@@ -4,7 +4,7 @@ import { TRANSLATION_MODULE } from "@medusajs/medusa/translation";
 export default async function ({ container }: ExecArgs) {
   const localeModuleService = container.resolve(TRANSLATION_MODULE);
   await localeModuleService.createLocales({
-    code: "uz-Uz",
+    code: "uz-UZ",
     name: "Uzbek (Uzbekistan)",
   });
 }
